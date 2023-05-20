fa76e7ae5284215a93cd408ff2ba1f1e84ec7ba2
movahediz <48517707+movahediz@users.noreply.github.com>
2023-05-20 13:15:55 +0200

    Update setup.sh
    comments added

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,6 +5,9 @@ echo "****************************************"
 
 echo "Updating package manager..."
 sudo add-apt-repository -y ppa:deadsnakes/ppa
+# assume yes for all queries : -y
+# Advanced Package Tool, more commonly known as APT, is a collection of tools used to install, update, remove, and otherwise manage software packages on Debian and its derivative operating systems, including Ubuntu and Linux Mint.
+# The deadsnakes PPA lets you install multiple Python versions on your Ubuntu system
 
 echo "Installing Python 3.9 and Virtual Environment"
 sudo apt-get update
@@ -25,6 +28,21 @@ echo "# DevOps Capstone Project additions" >> ~/.bashrc
 echo "export GITHUB_ACCOUNT=$GITHUB_ACCOUNT" >> ~/.bashrc
 echo 'export PS1="\[\e]0;\u:\W\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "' >> ~/.bashrc
 echo "source ~/venv/bin/activate" >> ~/.bashrc
+# the echo command to append lines to the end of .bashrc script
+# export is used to set environment variable in operating system
+# PS1 is the primary prompt which is displayed before each command
+# And the statement: \[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ dictates how the prompt is going to look like 
+# \033[36m] = Cyan
+# \u = Username of the current user
+# [\033[m] = Reset all styles and colors
+# @ = '@' character
+# [\033[32m] = Green
+# \h = The hostname
+# : = ':' character
+# [\033[33;1m] = Yellow(bold)
+# \w = The current working directory, with $HOME abbreviated with a tilde(~)
+# $ = Show '#' if the user ID of a user is 0, otherwise show '$' character
+
 
 echo "Installing Python depenencies..."
 source ~/venv/bin/activate && python3 -m pip install --upgrade pip wheel
